0eef210a7e77f6d958fed2d2e78942525e7c1120
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2023-12-03 16:10:30 +0100

    [Day 3] Solved part 2
    Solution is QUITE a bit janky because we started doing Part 1 inversely, but yeah

--- sol_snake/day_3.py
+++ sol_snake/day_3.py
@@ -51,23 +51,51 @@ def parse_input(content: TextIO) -> tuple[list[PartNumber], list[Symbol]]:
 
     return parts, symbols
 
-def is_adjacent(part : PartNumber, coords : list[Coordinate]) -> bool:
+def part_is_adjacent(part : PartNumber, coords : list[Coordinate]) -> bool:
+    # numbers are only horizontal, so y is always the same
+    y = part.start_pos[1]
     for i in range(part.range):
-        curr_x = part.start_pos[0]+i
-        curr_y = part.start_pos[1]
-        for d in Direction:
-            if get_coords((curr_x,curr_y), d) in coords:
-                return True
+        # x changes because of the number position
+        x = part.start_pos[0]+i
+        any_adjacent, _ = find_number_adjacent((x,y), coords)
+        if any_adjacent:
+            return True
     return False
 
-def day_3(content: TextIO, is_example: bool = False) -> int:
+def find_number_adjacent(pos : Coordinate, coords : list[Coordinate]) -> tuple[bool, list[Coordinate]]:
+    adjacent_coords = [get_coords(pos,d) for d in Direction if (get_coords(pos,d) in coords)]
+    return (adjacent_coords != [], adjacent_coords)
+
+def day_3(content: TextIO, is_example: bool = False) -> tuple[int, int]:
 
     all_parts, all_symbols = parse_input(content)
-    valid_part_numbers = [0]
+    valid_part_numbers = []
 
+    # For part 1, we don't really about which specific symbol
+    symbols_coords = [i.pos for i in all_symbols]
+    # first part
     for part in all_parts:
-        # so far, for part 1, we don't really about which specific symbol
-        symbols_coords = [i.pos for i in all_symbols]
-        if is_adjacent(part, symbols_coords):
+        # Part 1
+        if part_is_adjacent(part, symbols_coords):
             valid_part_numbers.append(part.value)
-    return sum(valid_part_numbers)
+
+    # Part 2, gear ratios
+    gear_ratios = []
+    valid_symbols = [i for i in all_symbols if i.value == "*"]
+    # this is a very ugly solution because our Part 1 solution is inverted from part 2, so we need an unique way of identifying the numbers to be unique and not duplicating...
+    parts_coord_map = {
+        (p.start_pos[0]+i, p.start_pos[1]) : p.value
+        for p in all_parts for i in range(p.range)
+    }
+
+    for i in valid_symbols:
+        _, adjacent_coords = find_number_adjacent(i.pos, list(parts_coord_map.keys()))
+        # this is very likely to fail on very specific input, since we're relying on non-duplicates
+        numbers_adjacent = list(set([parts_coord_map[j] for j in adjacent_coords]))
+
+        if len(numbers_adjacent) == 2:
+            first, second = numbers_adjacent
+            ratio = first * second
+            gear_ratios.append(ratio)
+
+    return sum(valid_part_numbers), sum(gear_ratios)
