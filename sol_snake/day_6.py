21d557bf0fe0c59c9bacbdebc347fed09b3c061a
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2023-12-06 23:22:25 +0100

    [Day 6] Added solution with proper input reading + refactored
    Who knew input reading would make such a big diff
    
    Also, transformed solution into functions because it's easier

--- sol_snake/day_6.py
+++ sol_snake/day_6.py
@@ -2,26 +2,42 @@ from typing import TextIO
 
 import numpy as np
 
+from utils import parse_ints
+
+
+def calculates_distance(button_time: int, race_duration: int) -> int:
+    # the more time we spend holding the button, the less we have
+    remaining_duration = race_duration - button_time
+    # we get 1 speed per 1 unit of holding down the button
+    speed = button_time * 1
+    # distance travelled
+    return speed * remaining_duration
+
+
+def solve(race_duration: int, record_distance: int) -> int:
+    return sum([
+        1 if calculates_distance(i, race_duration) > record_distance
+        else 0
+        for i in range(1, race_duration)
+    ])
+
 
 def day_6(content: TextIO, is_example: bool = False) -> tuple[int, int]:
-    # part 1
-    races_information = [
-        (7, 9), # duration (ms), distance (mm)
-        (15,40),
-        (30, 200),
-    ]
+    time_line: list[str]
+    distance_line: list[str]
+    with content as f:
+        # For part 1, input is
+        # Time : Number Number
+        # Distance : Number Number
+        time_line = f.readline().split(":")[1].strip().split(" ")
+        distance_line = f.readline().split(":")[1].strip().split(" ")
 
-    # part 2
-    races_information =[
-        (54817088, 446129210351007),
-    ]
-    ways_per_race = []
-    for duration, record in races_information:
-        ways_of_beating_record = 0
-        for button_time in range(1, duration):
-            remaining_duration = duration - button_time
-            distance_traveled = button_time * remaining_duration
-            if (distance_traveled > record):
-                ways_of_beating_record += 1
-        ways_per_race.append(ways_of_beating_record)
-    return np.prod(ways_per_race), 0
+    # part 1, races are separated by spaces
+    races_info_part1 = list(zip(parse_ints(time_line), parse_ints(distance_line)))
+
+    solutions_part1 = [solve(d, r) for (d, r) in races_info_part1]
+    # part 2, we need to assume it's all the same number, so
+    races_info_part2 = [(int("".join(time_line)), int("".join(distance_line)))]
+    solutions_part2 = [solve(d, r) for d, r in races_info_part2]
+
+    return np.prod(solutions_part1), solutions_part2[0]
