60c5806bf45b963452c4e5bb4fb8f44bb9baea52
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2023-12-08 18:10:58 +0100

    [Day 8] Added solution for part 1
    Pretty standard search, it will probably be extended into ranges though, so not sure if i'll  be able to make generic

--- sol_snake/day_8.py
+++ sol_snake/day_8.py
@@ -2,4 +2,36 @@ from typing import TextIO
 
 
 def day_8(content: TextIO, is_example: bool = False) -> tuple[int, int]:
-    return 0, 0
+    instructions = []
+    nodes_map : dict[str, tuple[str,str]] = {}
+
+    with content as f:
+        instructions = f.readline().strip()
+        f.readline()
+        for line in f:
+            line = line.strip()
+            print(line)
+            # AAA = (BBB, CCC)
+            pos, moves = line.strip().split("=")
+            # TNX
+            pos = pos.strip()
+            l, r = moves.strip("() ").split(", ")
+            moves = (l,r)
+            nodes_map[pos] = moves
+
+    steps_taken = 0
+    n_instructions = len(instructions)
+
+    start_node = "AAA"
+    end_node = "ZZZ"
+    curr_node = start_node
+    while curr_node != end_node:
+        left, right = nodes_map[curr_node]
+        next_instruction = instructions[steps_taken % n_instructions]
+        if next_instruction == "R":
+            curr_node = right
+        elif next_instruction == "L":
+            curr_node = left
+
+        steps_taken += 1
+    return steps_taken, 0
