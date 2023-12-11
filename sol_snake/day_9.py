c362fe97e73022dc7955f4afed45f4d7b81c47de
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2023-12-11 23:00:53 +0100

    [Day 9] Added naive solution for part 2
    It's just inverting and turning into deque.
    We used deque at the wrong level, but hey, it's faster than lists so not half bad
    
    We could definitely optimize it to be able to do it simultaneously (or at least, generically), but meh

--- sol_snake/day_9.py
+++ sol_snake/day_9.py
@@ -9,9 +9,11 @@ def difference(a : tuple[int,int]) -> int:
 
 
 def build_differences(history : list[int]) -> deque[list[int]]:
+    # so we don't modify it
+    start_history = [i for i in history]
     report = deque()
-    report.append(history)
-    to_check = [history]
+    report.append(start_history)
+    to_check = [start_history]
     while to_check != []:
         curr = to_check.pop()
         diff = list(map(difference, zip(curr[:-1], curr[1:])))
@@ -37,6 +39,23 @@ def extrapolate(report : deque[list[int]]) -> int:
     predicted_value = report[0][-1]
     return predicted_value
 
+def extrapolate_left(report : deque[list[int]]) -> int:
+    # turn the report into deques so we can use append left
+    report_but_deques = [deque(i) for i in report]
+    # le switcheroo to make the iteration easy
+    report_but_deques.reverse()
+    # initialize
+    last_val = 0
+    for i in range(len(report_but_deques)):
+        curr_line = report_but_deques[i]
+        next_val = curr_line[0]
+        predicted = next_val - last_val
+        curr_line.appendleft(predicted)
+        last_val = predicted
+
+    report_but_deques.reverse()
+    predicted_value = report_but_deques[0][0]
+    return predicted_value
 
 def day_9(content: TextIO, is_example: bool = False) -> tuple[int, int]:
 
@@ -45,10 +64,16 @@ def day_9(content: TextIO, is_example: bool = False) -> tuple[int, int]:
         histories = [parse_ints(line.strip()) for line in f]
 
     predictions = []
+
     for history in histories:
         report = build_differences(history)
         predict = extrapolate(report)
         predictions.append(predict)
 
-    #print(predictions)
-    return sum(predictions)
+    predictions_left = []
+    for history in histories:
+        report = build_differences(history)
+        predict_left = extrapolate_left(report)
+        predictions_left.append(predict_left)
+
+    return sum(predictions), sum(predictions_left)
