c454253b785c3af78b6efd4c87b6619454d5c204
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2023-12-15 14:28:16 +0100

    [Day 15] Added solution for part 2
    The way it was a written was a bit confusing, so also added a txt with something I used to understand

--- sol_snake/day_15.py
+++ sol_snake/day_15.py
@@ -1,3 +1,5 @@
+from collections import deque
+from dataclasses import dataclass
 from typing import TextIO
 
 
@@ -14,12 +16,73 @@ def hash_algo(s) -> int:
         # Set the current value to the remainder of dividing itself by 256.
         hashed_val = hashed_val % 256
     return hashed_val
+
+@dataclass
+class Lense:
+    label : str
+    focal_length : int
+    def __repr__(self) -> str:
+        return f"[{self.label} {self.focal_length}]"
+
 def day_15(content: TextIO, is_example: bool = False) -> tuple[int, int]:
 
-    hashes = []
+    steps = []
     with content as f:
-        for line in f.read().strip().split(","):
-            hashed = hash_algo(line)
-            hashes.append(hashed)
+        steps = [line for line in f.readline().strip().split(",")]
 
-    return sum(hashes), 0
+    # part 1
+    hashes_only = [hash_algo(i) for i in steps]
+
+    # part 2
+    type Box = deque[Lense]
+    storage : dict[int, Box] = {}
+    # pre parsing isn't necessary, just makes the actual steps logic cleaner
+    parsed_steps = []
+    for step in steps:
+        new_step = None
+        if "=" in step:
+            label, focal_length = step.split("=")
+            new_step = (label,"=", int(focal_length))
+        elif "-" in step:
+            label, _ = step.split("-")
+            new_step = (label, "-")
+        else:
+            continue
+        parsed_steps.append(new_step)
+
+    for step in parsed_steps:
+        label = step[0]
+        operation = step[1]
+        relevant_box = hash_algo(label)
+        # edge case, initializing the deque
+        if relevant_box not in storage:
+            storage[relevant_box] = deque()
+
+        curr_box = storage[relevant_box]
+
+        try:
+            labeled_lense = [i for i in curr_box if i.label == label][0]
+        except IndexError:
+            labeled_lense = None
+
+        if operation == "-":
+            if labeled_lense is not None:
+                curr_box.remove(labeled_lense)
+        elif operation == "=":
+            focal_length = step[2]
+            if labeled_lense is not None:
+                labeled_lense.focal_length = focal_length
+            else:
+                labeled_lense = Lense(label, focal_length)
+                curr_box.append(labeled_lense)
+
+    focusing_power = []
+    for box, lenses in storage.items():
+        box_power = box+1
+        for slot in range(len(lenses)):
+            slot_power = slot + 1
+            lense = lenses[slot]
+            focus_power = box_power * slot_power * lense.focal_length
+            focusing_power.append(focus_power)
+
+    return sum(hashes_only), sum(focusing_power)
