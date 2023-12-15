a7bab9e7361cab10fe1b197c67f6b6ecc77956bd
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2023-12-15 13:08:07 +0100

    [Day 11] Refactoring the code for readability
    No need to do the for loop 2x for the distance, might as well just make a function out of that
    Also moved the logic for part 1 and 2 to each of their functions

--- sol_snake/day_11.py
+++ sol_snake/day_11.py
@@ -3,27 +3,98 @@ from typing import TextIO
 import numpy as np
 import numpy.typing as nptypes
 
+from datatypes import Coordinate
 
-def find_indices_to_expand(matrix : nptypes.NDArray):
+
+def find_indices_to_expand(matrix: nptypes.NDArray):
     # lines that are fully 0s (aka, summing over columns will be 0)
-    line_indexes = np.nonzero(matrix.sum(axis=1) == 0)[0] # we don't care about the type
+    line_indexes = np.nonzero(matrix.sum(axis=1) == 0)[
+        0
+    ]  # we don't care about the type
     # columns that are fully 0s (aka, summing over lines will be 0)
-    col_indexes = np.nonzero(matrix.sum(axis=0) == 0)[0] # we don't care about the type
+    col_indexes = np.nonzero(matrix.sum(axis=0) == 0)[0]  # we don't care about the type
 
     return line_indexes, col_indexes
-def expand_universe(u_m :  nptypes.NDArray) -> nptypes.NDArray:
+
+
+def expand_universe(u_m: nptypes.NDArray) -> nptypes.NDArray:
     line_indexes, col_indexes = find_indices_to_expand(u_m)
     # insert the lines as 0
     expanded_lines = np.insert(u_m, line_indexes, 0, axis=0)
     # insert the news colums at those indexes with 0
     return np.insert(expanded_lines, col_indexes, 0, axis=1)
 
+
+def naive_find_expanded_coordinates(matrix: nptypes.NDArray) -> list[Coordinate]:
+    # Expand the universe
+    expanded = expand_universe(matrix)
+    # Find non zero values
+    galaxies = np.nonzero(expanded)
+    # Get their coordinates
+    expanded_coords = np.rec.fromarrays([galaxies[0], galaxies[1]]).tolist()
+    return expanded_coords
+
+
+def smart_find_expanded_coordinates(
+    matrix: nptypes.NDArray, expand_by: int = 2
+) -> list[Coordinate]:
+    """
+    Parameters
+    ----------
+    matrix : nptypes.NDArray
+        Our universe
+    expand_by : int, optional
+        How much larger the empty space really is, by is 2x the size
+
+    Returns
+    -------
+    list[Coordinate]
+        List of coordinates of the galaxies in the expanded universe
+    """
+
+    # We need to be a little smarter about it, since it's perhaps not a good idea to do insertion 1M times
+    # Instead of expanding the matrix, just get the coordinates directly
+    galaxies = np.nonzero(matrix)
+    coordinates_orig = np.rec.fromarrays([galaxies[0], galaxies[1]]).tolist()
+
+    # find which lines/columns will be neededed to expand
+    lines_i, cols_i = find_indices_to_expand(matrix)
+
+    # doing -1 since we need to account for the galaxies already in there
+    expansion_factor = expand_by - 1
+
+    expanded_coords = []
+    for x, y in coordinates_orig:
+        new_x = x
+        new_y = y
+        # count how many expansions (X-coord shifts) we'll have to make
+        lines = sum([1 if x > i else 0 for i in lines_i])
+        if lines != 0:
+            new_x = x + lines * expansion_factor
+
+        # do the same for the columns (Y-Coord shifts)
+        cols = sum([1 if y > i else 0 for i in cols_i])
+        if cols != 0:
+            new_y = y + cols * expansion_factor
+        expanded_coords.append((new_x, new_y))
+    return expanded_coords
+
+
 def manhattan_distance(a, b):
-    #print(a,b)
-    return abs(a[0]-b[0]) + abs(a[1]-b[1])
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+def calculate_distances(coordinates: list[Coordinate]) -> list[int]:
+    total_coords = len(coordinates)
+    return [
+        manhattan_distance(coordinates[i], coordinates[j])
+        for i in range(total_coords)
+        for j in range(i, total_coords)
+        if coordinates[i] != coordinates[j]
+    ]
+
 
 def day_11(content: TextIO, is_example: bool = False) -> tuple[int, int]:
-
     universe_matrix = []
     galaxy_count = 1
     with content as f:
@@ -39,46 +110,11 @@ def day_11(content: TextIO, is_example: bool = False) -> tuple[int, int]:
             universe_matrix.append(le_numbers)
 
     u_m = np.asarray(universe_matrix)
-    expanded = expand_universe(u_m)
-    galaxies = np.nonzero(expanded)
-    coordinates = np.rec.fromarrays([galaxies[0], galaxies[1]]).tolist()
-
-    lowest_distances = []
-
-    for i in range(len(coordinates)):
-        coords_a = coordinates[i]
-        distances = [manhattan_distance(coords_a, coordinates[j]) for j in  range(i, len(coordinates)) if coords_a != coordinates[j]]
-        lowest_distances.extend(distances)
-
+    coordinates_part1 = naive_find_expanded_coordinates(u_m)
+    distances_part1 = calculate_distances(coordinates_part1)
 
     # part 2
-    # We need to be a little smarter about it, since it's perhaps not a good idea to do insertion 1M times
-    non_expanded_galaxies = galaxies = np.nonzero(u_m)
-    lines_i, cols_i = find_indices_to_expand(u_m)
-    non_expanded_coords = np.rec.fromarrays([non_expanded_galaxies[0], non_expanded_galaxies[1]]).tolist()
+    coordinates_part2 = smart_find_expanded_coordinates(u_m, expand_by=1000 * 1000)
+    distances_part2 = calculate_distances(coordinates_part2)
 
-    # How large galaxies really are
-    times_larger = 1000*1000
-    # doing -1 since we need to account for the galaxy already in there
-    expansion_factor = times_larger - 1
-    new_coords = []
-    for x,y in non_expanded_coords:
-        new_x = x
-        new_y = y
-        # count how many empty lines were found for this x
-        lines = sum([1 if x > i else 0 for i in lines_i])
-        if lines != 0:
-            new_x = x + lines * expansion_factor
-
-        cols = sum([1 if y > i else 0 for i in cols_i])
-        if cols != 0:
-            new_y = y + cols * expansion_factor
-        new_coords.append((new_x, new_y))
-
-    lowest_distances_part2 = []
-    for i in range(len(new_coords)):
-        coords_a = new_coords[i]
-        distances = [manhattan_distance(coords_a, new_coords[j]) for j in  range(i, len(new_coords)) if coords_a != new_coords[j]]
-        lowest_distances_part2.extend(distances)
-
-    return sum(lowest_distances), sum(lowest_distances_part2)
+    return sum(distances_part1), sum(distances_part2)
